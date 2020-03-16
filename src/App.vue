8b05117fdeb8994c85e130ac4a8e940fe8a03872
= <felix.bruening@sumcumo.com>
2020-03-16 16:05:48 +0100

    feat: initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,72 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p v-if="isConnected">We're connected to the server!</p>
+    <p>Message from server: "{{ socketMessage }}"</p>
+    <div class="row">
+      <div class="user" v-for="user in users" :key="user.id">
+        <h5>{{ user.id }}</h5>
+        <h3>{{ user.estimation }}</h3>
+      </div>
+    </div>
+    <button @click="pingServer()">Ping Server</button>
+
+    <button v-for="number in numbers" :key="number" @click="estimate(number)">
+      {{ number }}
+    </button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+
+  data() {
+    return {
+      isConnected: false,
+      socketMessage: '',
+      values: [],
+      users: {},
+      numbers: [1, 2, 3, 5, 8, 13, 21],
+    }
+  },
+
+  sockets: {
+    connect() {
+      // Fired when the socket connects.
+      this.isConnected = true
+    },
+
+    disconnect() {
+      this.isConnected = false
+    },
+
+    // Fired when the server sends something on the "messageChannel" channel.
+    messageChannel(data) {
+      this.socketMessage = data
+    },
+
+    estimatedValues(values) {
+      this.values = values
+    },
+
+    userList(users) {
+      console.log(users)
+      this.users = users.sort((a, b) => a > b)
+    },
+  },
+
+  methods: {
+    pingServer() {
+      // Send the "ping" event to the server.
+      console.log('sending ping...')
+      this.$socket.client.emit('pingServer', 'PING!')
+    },
+
+    estimate(value) {
+      console.log('estimate', value)
+      this.$socket.client.emit('estimate', value)
+    },
+  },
 }
 </script>
 
@@ -25,4 +79,15 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.row {
+  display: flex;
+}
+
+  .row div {
+    flex: 1;
+    border: 1px solid black;
+    border-radius: 5px;
+    margin: 1rem;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
+import socketClient from 'socket.io-client'
+import VueSocketIOExt from 'vue-socket.io-extended'
+
 import App from './App.vue'
+
+export const SocketInstance = socketClient('http://localhost:3000')
+
+Vue.use(VueSocketIOExt, SocketInstance)
 
 Vue.config.productionTip = false
 
