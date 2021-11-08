06bf50acca74a6fe3153582ed4d808630225c1e5
Yannic Inselmann <yannic@wtfoxtrot.de>
2021-11-08 14:39:53 +0100

    fix: counting of clients

--- packages/backend/src/rooms.ts
+++ packages/backend/src/rooms.ts
@@ -1,4 +1,5 @@
 import { Room } from '@estimate-me/api'
+import { Server } from 'socket.io'
 import db from './db'
 
 export const rooms = db.addCollection<Room>('rooms')
@@ -7,10 +8,11 @@ export const findOrCreateRoom = (roomName: string) => {
   return rooms.findOne({ name: roomName }) ?? rooms.insert({ name: roomName, estimationsVisible: false, estimationValues: [] })!
 }
 
-export const numClientsInRoom = (io: any, roomId: string) => {
-  const room = io.sockets.adapter.rooms[roomId]
+export const numClientsInRoom = (io: Server, roomId: string) => {
+  console.log(io.sockets.adapter.rooms)
+  const room = io.sockets.adapter.rooms.get(roomId)
   if (!room) {
     return -1
   }
-  return room.length
+  return room.size
 }
